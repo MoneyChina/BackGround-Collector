88181405903824c68ccd159266155d51266cb6c0
ZHOU LEI <money-8888@outlook.com>
2019-06-01 15:26:33 +0800

    Update mongodb connection

--- mongoose.js
+++ mongoose.js
@@ -6,8 +6,8 @@ var mongoose = require('mongoose')
 //         console.log('连接成功');
 //     }
 // }) //服务器
-mongoose.connect('mongodb://controller:cccon@problem-kid.top:20006/app', { useNewUrlParser: true },  function(err){
-//mongoose.connect('mongodb://controller:cccon@localhost:20006/app', { useNewUrlParser: true },  function(err){
+
+mongoose.connect('mongodb://controller:cccon@localhost:20006/app', { useNewUrlParser: true },  function(err){
     if(err){
         console.log('连接失败');
     }else{
